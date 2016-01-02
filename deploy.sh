2f16d592ba26c3b5aa4047bdb2ac7c23df514214
Joseph Mambwe <josephmambwe@me.com>
2016-01-02 15:38:48 +0000

    Fixed deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 jekyll build
 git branch -D master
 git checkout -b master
+sed -i "/\b\(_site\|rat\)\b/d" filename
 git filter-branch --subdirectory-filter _site/ -f
 git checkout source
-git push origin :master
 git push --all origin
