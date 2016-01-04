541227b1698676680f8ed8cec3fef2a194740128
Joseph Mambwe <josephmambwe@me.com>
2016-01-04 12:34:27 +0000

    amended

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 jekyll build
 git branch -D master
+git push origin :master
 git checkout -b master
-sed -i "/\b\(_site\|rat\)\b/d" filename
+sed -i "/\b\(_site\)\b/d" filename
 git filter-branch --subdirectory-filter _site/ -f
 echo 'docs.terrestrial.io' > CNAME
 git checkout source
