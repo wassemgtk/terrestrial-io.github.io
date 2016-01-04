72785b2f3ccce332666f3ce0e59f11798f2876e3
Joseph Mambwe <josephmambwe@me.com>
2016-01-04 09:24:06 +0000

    Added CNAME file creation

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,6 @@ git branch -D master
 git checkout -b master
 sed -i "/\b\(_site\|rat\)\b/d" filename
 git filter-branch --subdirectory-filter _site/ -f
+echo 'docs.terrestrial.io' > CNAME
 git checkout source
 git push --all origin
