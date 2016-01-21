f7edc61a8f883aefd2797b746884a36f767b5405
Joseph Mambwe <josephmambwe@me.com>
2016-01-21 13:48:17 +0000

    committ

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,10 @@
 jekyll build
-git branch -D master
-git push origin :master
-git checkout -b master
-#sed -i "/\b\(_site\)\b/d" 
 cd _site/
 echo 'docs.terrestrial.io' > CNAME
 cd ..
+git branch -D master
+git push origin :master
+git checkout -b master
 git filter-branch --subdirectory-filter _site/ -f
 git checkout source
 git push --all origin
