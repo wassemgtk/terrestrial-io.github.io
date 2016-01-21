ffe2ae40586c10e61e0d6a591f408c19c2abb33e
Joseph Mambwe <josephmambwe@me.com>
2016-01-21 14:04:18 +0000

    I see no changes

--- _site/sections/development/shared/git-changes.md
+++
@@ -1,2 +0,0 @@
-
-> Note: If you work in git, we recommend that you stage a commit before clicking finish so that you can revert any adverse changes that may occur.
--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,8 @@ cd ..
 git branch -D master
 git push origin :master
 git checkout -b master
-git add -A
+
+git add _site
 git commit -m "Added _site folder"
 git filter-branch --subdirectory-filter _site/ -f
 git checkout source
