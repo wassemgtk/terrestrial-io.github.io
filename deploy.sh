fe6bdfd3c5b1c013ac03a8217b1f5136a2a09fbb
Joseph Mambwe <josephmambwe@me.com>
2016-01-21 12:21:57 +0000

    Edit to deploy script

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,7 @@ git checkout -b master
 sed -i "/\b\(_site\)\b/d" filename
 git filter-branch --subdirectory-filter _site/ -f
 echo 'docs.terrestrial.io' > CNAME
+git add -A
+git commit -m "Added CNAME"
 git checkout source
 git push --all origin
