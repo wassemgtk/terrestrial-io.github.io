fd5240f32b8a93113ade1c6b4c70d79d0e32584f
Joseph Mambwe <josephmambwe@me.com>
2016-01-21 12:27:01 +0000

    edits to deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,8 @@ jekyll build
 git branch -D master
 git push origin :master
 git checkout -b master
+echo 'docs.terrestrial.io' > _site/CNAME
 sed -i "/\b\(_site\)\b/d" filename
 git filter-branch --subdirectory-filter _site/ -f
-echo 'docs.terrestrial.io' > CNAME
-git add -A
-git commit -m "Added CNAME"
 git checkout source
 git push --all origin
